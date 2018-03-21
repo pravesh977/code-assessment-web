bfc658d0e9b815e0db77302fa816352827a7648c
Pravesh Shrestha <praveshshrestha@Praveshs-MacBook-Pro.local>
2018-03-20 21:45:43 -0400

    first commit added stylesheet and fontawesome

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import ProductsContainer from './ProductsContainer'
-import CartContainer from './CartContainer'
-
+import React from 'react';
+import ProductsContainer from './ProductsContainer';
+import CartContainer from './CartContainer';
+// import './../../node_modules/react-scripts/template/src/App.css';
+import './Styles.css';
 const App = () => (
   <div>
-    <h2>Shopping Cart Example</h2>
+    <div className="headersdiv">
+      <h2>ACME STORE</h2>
+      <CartContainer />
+    </div>
     <hr/>
     <ProductsContainer />
+    <h1><i className="fas fa-shopping-cart"></i></h1>
     <hr/>
-    <CartContainer />
   </div>
 )
 
